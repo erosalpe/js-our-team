1650ef4763583f59b4f5aedb2ecdf36a2fefe259
erosalpe <eros.alpegiani@gmail.com>
2024-01-21 17:32:28 +0100

    feat(main) aggiunte cards con immaginni e dati

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,88 @@ const arrayTeam = [
     {
         nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        immagine: "wayne-barnett-founder-ceo.jpg"
+        immagine: "wayne-barnett-founder-ceo.jpg",
     },
     {
         nome: "Angela Caroll",
         ruolo: "Chief Editor",
-        immagine: "angela-caroll-chief-editor.jpg"
+        immagine: "angela-caroll-chief-editor.jpg",
     },
     {
         nome: "Walter Gordon",
         ruolo: "Office Manager",
-        immagine: "walter-gordon-office-manager.jpg"
+        immagine: "walter-gordon-office-manager.jpg",
     },
     {
         nome: "Angela Lopez",
         ruolo: "Social Media Manager",
-        immagine: "angela-lopez-social-media-manager.jpg"
+        immagine: "angela-lopez-social-media-manager.jpg",
     },
     {
         nome: "Scott Estrada",
         ruolo: "Developer",
-        immagine: "scott-estrada-developer.jpg"
+        immagine: "scott-estrada-developer.jpg",
     },
     {
         nome: "Barbara Ramos",
         ruolo: "Graphic Designer",
-        immagine: "barbara-ramos-graphic-designer.jpg"
+        immagine: "barbara-ramos-graphic-designer.jpg",
     }
 
 ]
 
+const nomeHtml = document.getElementById("nome");
+
+const grigliaCardsHtml = document.getElementById("grigliaCarte");
+
+
 for(let i = 0; i < arrayTeam.length; i++){
-    console.log(arrayTeam[i]);
     let elemento = arrayTeam[i];
+    let elementoImmagine = elemento.immagine;
+    const cardFigure = document.createElement("figure");
+    const figureImg = document.createElement("img");
+    cardFigure.appendChild(figureImg);
+    figureImg.setAttribute("src", `./assets/img/` + `${elementoImmagine}`);
+    figureImg.setAttribute("alt", "profile-picture");
+    figureImg.setAttribute("id", "imageId");
+
+    /*
     document.writeln(`${elemento.nome}`);
     document.writeln(`${elemento.ruolo}`);
-    document.writeln(`${elemento.immagine}`);
+    document.writeln(`${elemento.immagine}`);*/
+
+
+    const card = document.createElement("div");
+    const cardName = document.createElement("span");
+    const cardJob = document.createElement("span");
+    card.classList.add("card");
+    cardName.setAttribute("id", "nome");
+    cardJob.setAttribute("id", "ruolo");
+    
+
+
+
+    
+    
+    
+    card.appendChild(cardFigure);
+    card.appendChild(cardName);
+    card.appendChild(cardJob);
+    
+    
+
+    grigliaCardsHtml.appendChild(card);
+    console.log(elementoImmagine);
+
+    cardName.innerText = elemento.nome;
+    cardJob.innerText = elemento.ruolo;
+    
+    
+    
+
+
+
+
+
+    
 }
